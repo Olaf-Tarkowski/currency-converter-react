d5e8890fdebaa7a18a5249db1643271a77b589cf
Olaf Tarkowski <olaftarkowski97@gmail.com>
2021-09-28 17:53:24 +0200

    Create useCurrentDate component and impor to Clock

---
+++ src/Clock/useCurrentDate.js
@@ -0,0 +1,12 @@
+import { useEffect, useState } from "react";
+
+export const useCurrentDate = () => {
+  const [date, setDate] = useState(new Date());
+  useEffect(() => {
+    setInterval(() => {
+      setDate(new Date());
+    });
+  }, []);
+
+  return date;
+};
