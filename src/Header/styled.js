fde257d210a0320b29bb7256c8647d31021ba66c
Olaf Tarkowski <olaftarkowski97@gmail.com>
2021-10-03 21:53:45 +0200

    Changed header color

--- src/Header/styled.js
+++ src/Header/styled.js
@@ -8,12 +8,12 @@ export const StyledHeader = styled.header`
 
 export const Title = styled.h1`
     padding-top: 150px;
-    text-shadow: 2px 2px rgb(255, 255, 255);
-    color: rgb(168, 0, 0);
+    text-shadow: 2px 2px rgb(108 107 107);
+    color: #57609a;
 `;
 
 export const Paragraph = styled.p`
     margin-top: 70px;
-    text-shadow: 1px 1px rgb(0, 0, 0);
-    color: rgb(168, 0, 0);
+    text-shadow: 1px 1px rgb(108 107 107);
+    color: #57609a;
 `;
