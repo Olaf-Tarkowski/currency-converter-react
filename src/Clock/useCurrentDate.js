9693dd33ea0fdbc83cbc6e56f52514f9ffad37dc
Olaf Tarkowski <olaftarkowski97@gmail.com>
2021-10-03 17:30:52 +0200

    adding clearInterval

--- src/Clock/useCurrentDate.js
+++ src/Clock/useCurrentDate.js
@@ -3,9 +3,11 @@ import { useEffect, useState } from "react";
 export const useCurrentDate = () => {
   const [date, setDate] = useState(new Date());
   useEffect(() => {
-    setInterval(() => {
+    const intervalId = setInterval(() => {
       setDate(new Date());
-    });
+    }, 1000);
+
+    clearInterval(intervalId);
   }, []);
 
   return date;
