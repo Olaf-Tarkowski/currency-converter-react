2b2f8dfc5d24fa0be0dd299128fb9421c5031ccb
Olaf Tarkowski <olaftarkowski97@gmail.com>
2021-10-04 21:40:50 +0200

    added new color

--- src/theme.js
+++ src/theme.js
@@ -2,6 +2,7 @@ export const theme = {
     colors: {
         waikawaGray: "#57609a",
         gray: "rgb(108 107 107)",
+        concrete: "#F2F2F2",
     },
     breakpoints: {
         mobile: 600
