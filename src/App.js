b8a2851b3a99bfaa52bb0eb44b2edef3c2dd5f61
Olaf Tarkowski <olaftarkowski97@gmail.com>
2021-10-04 20:58:35 +0200

    Added Footer title

--- src/App.js
+++ src/App.js
@@ -4,17 +4,19 @@ import Footer from "./Footer";
 import Container from "./Container";
 import Clock from "./Clock";
 import { ThemeProvider } from "styled-components";
-import {theme} from "./theme"
+import { theme } from "./theme"
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-    <Container>
-    <Header />
-    <Form />
-    <Clock />
-    <Footer />
-    </Container>
+      <Container>
+        <Header />
+        <Form />
+        <Clock />
+        <Footer>
+          YouCode
+        </Footer>
+      </Container>
     </ThemeProvider>
   );
 }
