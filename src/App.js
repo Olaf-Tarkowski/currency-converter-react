63479ab9ef791c06b5068a1e2ad6f3030c513953
Olaf Tarkowski <olaftarkowski97@gmail.com>
2021-10-04 21:40:39 +0200

    transferred theme to index.js

--- src/App.js
+++ src/App.js
@@ -3,21 +3,17 @@ import Header from "./Header";
 import Footer from "./Footer";
 import Container from "./Container";
 import Clock from "./Clock";
-import { ThemeProvider } from "styled-components";
-import { theme } from "./theme"
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <Container>
-        <Header />
-        <Form />
-        <Clock />
-        <Footer>
-          YouCode
-        </Footer>
-      </Container>
-    </ThemeProvider>
+    <Container>
+      <Header />
+      <Form />
+      <Clock />
+      <Footer>
+        YouCode
+      </Footer>
+    </Container>
   );
 }
 
