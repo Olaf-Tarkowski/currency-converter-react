cb8e4d5746fbab7bffe7bc74809d47616b68df30
Olaf Tarkowski <olaftarkowski97@gmail.com>
2021-10-24 13:28:35 +0200

    padding-top: 300px

--- src/Clock/styled.js
+++ src/Clock/styled.js
@@ -4,7 +4,7 @@ export const StyledClock = styled.p`
     font-family: 'Roboto Mono', monospace;
     font-size: 12px;
     margin-right: 19px;
-    padding-top: 380px;
+    padding-top: 300px;
     display: flex;
     justify-content: flex-end;
 
