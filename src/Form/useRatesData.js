6451820b8d17394045e4aaf732d0132a279fd011
Olaf Tarkowski <olaftarkowski97@gmail.com>
2021-10-24 13:06:24 +0200

    just good look

--- src/Form/useRatesData.js
+++ src/Form/useRatesData.js
@@ -1,28 +1,28 @@
 import { useState, useEffect } from "react";
 
 export const useRatesData = () => {
-    const base_url = "https://api.exchangerate.host/latest?base=PLN"
-    const [ratesData, setRatesData] = useState({
-      state: "loading",
-    });
-  
-    useEffect(() => {
-      const fetchApi = async () => {
-        try {
-          const response = await fetch(base_url);
-          if (!response.ok) {
-            throw new Error(response.statusText)
-          }
-          const { rates, date } = await response.json();
-          setRatesData({ state: "success", rates, date });
-  
-        } catch {
-          setRatesData({
-            state: "error",
-          });
+  const base_url = "https://api.exchangerate.host/latest?base=PLN"
+  const [ratesData, setRatesData] = useState({
+    state: "loading",
+  });
+
+  useEffect(() => {
+    const fetchApi = async () => {
+      try {
+        const response = await fetch(base_url);
+        if (!response.ok) {
+          throw new Error(response.statusText)
         }
-      };
-      setTimeout(fetchApi, 2_000);
-    }, []);
-    return ratesData;
-  };
+        const { rates, date } = await response.json();
+        setRatesData({ state: "success", rates, date });
+
+      } catch {
+        setRatesData({
+          state: "error",
+        });
+      }
+    };
+    setTimeout(fetchApi, 2_000);
+  }, []);
+  return ratesData;
+};
