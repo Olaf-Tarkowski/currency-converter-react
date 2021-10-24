19a73118702f1a627c7e9527a8d170ea33d14500
Olaf Tarkowski <olaftarkowski97@gmail.com>
2021-10-24 13:07:09 +0200

    delete currencies.js

--- src/Form/currencies.js
+++
@@ -1,6 +0,0 @@
-export const currencies = [
-    { name: "EUR", value: "4.5633" },
-    { name: "USD", value: "3.7736" },
-    { name: "GBP", value: "5.2474" },
-    { name: "CHF", value: "4.1282" },
-]
