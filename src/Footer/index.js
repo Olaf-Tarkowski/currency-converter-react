5052b75ea177f017024fd7cbe8c757f6422f04f2
Olaf Tarkowski <olaftarkowski97@gmail.com>
2021-10-24 13:18:24 +0200

    lock footer in one position

--- src/Footer/index.js
+++ src/Footer/index.js
@@ -1,7 +1,10 @@
 import styled from "styled-components"
 
 export const Footer = styled.footer`
+    position: fixed;
+    bottom: 0;
     padding: 7px;
+    width: 100%;
     font-size: 20px;
     color: rgb(255, 255, 255);
     background-color: rgb(11, 11, 11);
