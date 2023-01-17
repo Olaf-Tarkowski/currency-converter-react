d6a09c1c5af64c22329737284e4be6bb60e4149b
Olaf-Tarkowski <tarkowski.software@gmail.com>
2023-01-17 22:08:42 +0100

    initial commit

--- src/Clock/index.js
+++ src/Clock/index.js
@@ -1,23 +1,20 @@
-import { StyledClock } from "./styled"
-import { useCurrentDate } from "./useCurrentDate"
+import { StyledClock } from "./styled";
+import { useCurrentDate } from "./useCurrentDate";
 
-const time = (date) => date.toLocaleString("en-UK", {
-  weekday: "long",
-  day: "numeric",
-  month: "long",
-  hour: "numeric",
-  minute: "numeric",
-  second: "numeric",
-});
+const time = (date) =>
+  date.toLocaleString("en-UK", {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+    hour: "numeric",
+    minute: "numeric",
+    second: "numeric",
+  });
 
 const Clock = () => {
   const date = useCurrentDate();
 
-  return (
-    <StyledClock>
-      Today is {time(date)}
-    </StyledClock>
-  );
-}
+  return <StyledClock>Today is {time(date)}</StyledClock>;
+};
 
 export default Clock;
