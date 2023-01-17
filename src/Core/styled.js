2eaa23b50164456415182653872b5df7ee37e762
Olaf-Tarkowski <tarkowski.software@gmail.com>
2023-01-17 22:11:13 +0100

    styled por App added

---
+++ src/Core/styled.js
@@ -0,0 +1,19 @@
+import styled from "styled-components"
+
+export const Footer = styled.footer`
+    padding: 7px;
+    width: 100%;
+    font-size: 20px;
+    color: rgb(255, 255, 255);
+    background-color: rgb(11, 11, 11);
+    text-align: center;
+`;
+
+export const FooterContainer = styled.div`
+    width:100%;
+    position: fixed;
+    bottom: 0;
+`;
+
+
+export default Footer;
